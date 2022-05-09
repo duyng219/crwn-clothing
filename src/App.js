36e7af843822191fc41e4cee5b15f196ba9918b1
hding2196 <hding2196@gmail.com>
2022-05-09 09:02:29 +0700

    update header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.components';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
       <Route exact path='/' element={<HomePage/>}/>
       <Route path='/shop' element={<ShopPage/>}/>
